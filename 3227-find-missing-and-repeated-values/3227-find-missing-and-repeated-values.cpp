accc195c7e755ab47f0e40d33c535a3efe923865
Aditya_Kumar <116440438+Aditya-1166@users.noreply.github.com>
2025-06-23 11:56:43 +0530

    Time: 0 ms (100%), Space: 25.6 MB (85.48%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -2,25 +2,33 @@ class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
         int n = grid.size();
-        int size = n * n;
-        vector<int> count(size + 1, 0);
-
+        int n_squared = n * n;
+        
+        long long expected_sum = 1LL * n_squared * (n_squared + 1) / 2;
+        long long expected_sum_squares = 1LL * n_squared * (n_squared + 1) * (2 * n_squared + 1) / 6;
+        
+        long long actual_sum = 0, actual_sum_squares = 0;
+        
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                count[grid[i][j]]++;
+                actual_sum += grid[i][j];
+                actual_sum_squares += (long long)grid[i][j] * grid[i][j];
             }
         }
 
-        int a = -1, b = -1;
+        // a - b
+        long long diff_sum = actual_sum - expected_sum;
 
-        for (int num = 1; num <= size; num++) {
-            if (count[num] == 2) {
-                a = num;
-            } else if (count[num] == 0) {
-                b = num;
-            }
-        }
-
+        // a² - b²
+        long long diff_sum_squares = actual_sum_squares - expected_sum_squares;
+        
+        // a + b = (a² - b²) / (a - b)
+        long long sum_a_b = diff_sum_squares / diff_sum;
+        
+        // Now we can find a and b
+        int a = (sum_a_b + diff_sum) / 2;
+        int b = (sum_a_b - diff_sum) / 2;
+        
         return {a, b};
     }
 };
