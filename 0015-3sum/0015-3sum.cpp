54bfbd0582831584e60e91afba51767f91192be3
Aditya_Kumar <116440438+Aditya-1166@users.noreply.github.com>
2025-06-25 12:18:52 +0530

    Time: 43 ms (86.81%), Space: 29.1 MB (52.08%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> ans;
+        // Sort array to use two-pointer approach and handle duplicates easily
+        sort(nums.begin(),nums.end());
+
+        // Iterate through the array, fixing one element at a time -- nums[i]
+        for(int i=0;i<n;i++){
+            // two pointers j & k
+            int j=i+1,k=n-1;
+            if(i>0 && nums[i]== nums[i-1]) continue; // Skip duplicate
+            while(j<k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum<0){
+            // If the sum is too small, move the left pointer `j` to the right to increase the sum
+                    j++;
+                }
+                else if(sum>0){
+            // If the sum is too large, move the right pointer `k` to the left to decrease the sum
+                    k--;
+                }
+                else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+
+                    // Skip duplicate values for j to avoid repeated triplets
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+};
