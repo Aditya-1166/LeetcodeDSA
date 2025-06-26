2ebe983bb7415eac5025a2c015215a022eba710d
Aditya_Kumar <116440438+Aditya-1166@users.noreply.github.com>
2025-06-26 13:26:13 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.88%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        
+        int n=nums.size();
+        int pivot=-1;
+// step 1: To find pivot - first index i from the back of the given array where arr[i] becomes smaller than arr[i+1].
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                pivot=i;
+                break;
+            }
+        }
+// Step 2: If no pivot, reverse the entire array
+        if(pivot==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+// Step 3: Next Greater Element - Swap the right most elemnent greater then pivot
+        for(int i=n-1;i>pivot;i--){
+            if(nums[i] > nums[pivot]){
+                swap(nums[i],nums[pivot]);
+                break;
+            }
+        }
+// Step 4: Reverse entire array from pivot+i to n-1
+        int p=pivot+1,q=n-1;
+        while(p<q){
+            swap(nums[p++],nums[q--]);
+        }
     }
 };
